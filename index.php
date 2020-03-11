c3bce4275f05bbec6fb6d9e014c5760e0678748f
Grace Mites <gmites@metropolitan-touring.com>
2020-03-11 16:24:35 -0500

    Initial commit

---
+++ index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Single Page</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+  <link rel="stylesheet" href="./css/main.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+</head>
+<body>
+    <div class="container">
+        <h1>Single Page</h1>
+        <p>This is some text.</p>
+              
+        <table class="table table-striped">
+            <thead>
+                <tr>
+                <th scope="col">Id</th>
+                <th scope="col">Name</th>
+                <th scope="col">City</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <th scope="row">Grace</th>
+                    <td>Gracy</td>
+                    <td>Quito</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+    <footer class="sp-footer">
+        <div class="row text-center">
+            <div class="col-md-12">
+                <p>Contact information: <a href="mailto:grace.mites@gmail.com">grace.mites@gmail.com</a></p>
+                <p>Quito - Ecuador / All rights reserved </p>
+            </div>	
+        </div>	
+    </footer>   
+</body>
+</html>
